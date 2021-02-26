8122b154a1ade1d0bbc27c77b9980cc1b3e89085
Tom Caron <tcaron@flatiron.com>
2021-02-26 09:09:27 -0500

    AN-255: rendering smoe kind of form based on https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Forms

--- hello/urls.py
+++ hello/urls.py
@@ -5,3 +5,6 @@ from . import views
 urlpatterns = [    
     path('', views.hello, name='hello'),
 ]
+urlpatterns += [
+    path('book/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,42 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("this is the way!")
+    return HttpResponse("hello")
+
+
+import datetime
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from . import forms
+
+
+def renew_book_librarian(request):
+    # book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = forms.RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            # book_instance.due_back = form.cleaned_data['renewal_date']
+            # book_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed') )
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = forms.RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        # 'book_instance': book_instance,
+    }
+
+    return render(request, 'book_renew_librarian.html', context)
