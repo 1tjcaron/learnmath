16ea18c726f6a697beb868242671c520fc887072
Tom Caron <tcaron@flatiron.com>
2021-02-28 14:31:42 -0500

    AN_255:  Clean up the two views

--- hello/forms.py
+++ hello/forms.py
@@ -9,5 +9,5 @@ class ZeroPercForm(forms.Form):
     problem_two = forms.DecimalField(help_text="Find 100% of 60", decimal_places=0)
 
 class NumberLineForm(forms.Form):
-    problem_one = forms.DecimalField(help_text="Find 0% of 60", decimal_places=0)
-    # problem_two = forms.DecimalField(help_text="Find 100% of 60", decimal_places=0)
+    problem_one = forms.DecimalField(decimal_places=0, 
+        widget=forms.NumberInput(attrs={'style':'max-width: 40px; border-color:darkgoldenrod; border-radius: 10px;'}))
