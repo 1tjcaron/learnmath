ba177e65fec51374c7a50d946e0460b1807f8b6e
Tom Caron <tcaron@flatiron.com>
2021-03-07 15:10:28 -0500

    shorten the form field for the perc quiz

--- hello/forms.py
+++ hello/forms.py
@@ -5,8 +5,14 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 class ZeroPercForm(forms.Form):
-    problem_one = forms.DecimalField(help_text="Find 0% of 60", decimal_places=0)
-    problem_two = forms.DecimalField(help_text="Find 100% of 60", decimal_places=0)
+    problem_one = forms.DecimalField(
+        help_text="Find 0% of 60", 
+        widget=forms.NumberInput(attrs={'style':'max-width: 40px; border-color:darkgoldenrod; border-radius: 10px;'}),
+        decimal_places=0)
+    problem_two = forms.DecimalField(
+        help_text="Find 100% of 60", 
+        widget=forms.NumberInput(attrs={'style':'max-width: 40px; border-color:darkgoldenrod; border-radius: 10px;'}),
+        decimal_places=0)
 
 class NumberLineForm(forms.Form):
     problem_one = forms.DecimalField(decimal_places=0, 
