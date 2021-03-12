9a4e94ba9695332c2386f67723603bd157bd1180
Tom Caron <tcaron@flatiron.com>
2021-03-12 10:09:36 -0500

    AN-274: Bootstrap elements used a little more prominently

--- hello/forms.py
+++ hello/forms.py
@@ -4,16 +4,18 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
-
+NUM_STYLE ={'class':"form-control-inline", 'style':'max-width: 100px;'};
 class AnyPercForm(forms.Form):
     # https://stackoverflow.com/questions/871037/django-overriding-init-for-custom-forms
     problem_one = forms.DecimalField(
-        widget=forms.NumberInput(attrs={'style':'max-width: 50px; border-color:darkgoldenrod; border-radius: 10px;'}),
+        widget=forms.NumberInput(
+            attrs=NUM_STYLE),
         decimal_places=0,
         min_value=0,
         max_value=100)
     problem_two = forms.DecimalField(
-        widget=forms.NumberInput(attrs={'style':'max-width: 50px; border-color:darkgoldenrod; border-radius: 10px;'}),
+        widget=forms.NumberInput(
+            attrs=NUM_STYLE),
         decimal_places=0,
         min_value=0,
         max_value=100
@@ -27,6 +29,9 @@ class DefinedPercForm(AnyPercForm):
         self.fields["problem_two"].help_text = f"Find {p2}% of {n}"
 
 class NumberLineForm(forms.Form):
-    problem_one = forms.DecimalField(decimal_places=0, 
-        widget=forms.NumberInput(attrs={'style':'max-width: 40px; border-color:darkgoldenrod; border-radius: 10px;'}))
+    problem_one = forms.DecimalField(
+        decimal_places=0,
+        min_value=0,
+        max_value=100,
+        widget=forms.NumberInput(attrs=NUM_STYLE))
 
